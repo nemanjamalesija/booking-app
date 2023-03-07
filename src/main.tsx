89bc7c5046b9c01bd8fb2edb5100e71f3cf4a2b5
Nemanja Malesija <malesija13@gmail.com>
2023-03-07 05:15:27 +0100

    Setup headers, cors issue again

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   ApolloClient,
   InMemoryCache,
-  gql,
+  createHttpLink,
   ApolloProvider,
 } from '@apollo/client';
 import ReactDOM from 'react-dom/client';
@@ -11,9 +11,12 @@ import './index.css';
 
 const API_KEY =
   '1ar8ovS31mVFVx9TfppXKmc2JVqmZaA7ohsDUm8JFwD81CcGrfN_zcPpzvIfbThCe7r72BRzFEqIXIFBzxrJpQeaMw3xihNIjPeUXEBEwTtiTLexbIehbJdBAqgGZHYx';
+const httpLink = createHttpLink({
+  uri: 'https://api.yelp.com/v3/graphql',
+});
 
 const client = new ApolloClient({
-  uri: 'https://api.yelp.com/v3/graphql',
+  link: httpLink,
   cache: new InMemoryCache(),
   headers: {
     Authorization: `Bearer ${API_KEY}`,
