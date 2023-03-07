6ac9bde450138803c5d9a857ca07c8efa55619c9
Nemanja Malesija <malesija13@gmail.com>
2023-03-07 05:10:48 +0100

    Request query. Get cors issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
-import { useState } from 'react';
+import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_TEST_QUERY = gql`
+  query {
+    business(id: "garaje-san-francisco") {
+      name
+      id
+      alias
+      rating
+      url
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_TEST_QUERY);
+
+  console.log(data);
   return <div className="App"></div>;
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  ApolloProvider,
+} from '@apollo/client';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const API_KEY =
+  '1ar8ovS31mVFVx9TfppXKmc2JVqmZaA7ohsDUm8JFwD81CcGrfN_zcPpzvIfbThCe7r72BRzFEqIXIFBzxrJpQeaMw3xihNIjPeUXEBEwTtiTLexbIehbJdBAqgGZHYx';
+
+const client = new ApolloClient({
+  uri: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Bearer ${API_KEY}`,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
